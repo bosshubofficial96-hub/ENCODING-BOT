9b9c0e3bf4b0dd0e446185052aba61420fc7d3ad
Bosshub <bosshubofficial96@gmail.com>
2026-05-22 14:37:05 +0530

    Update start.py

--- VideoEncoder/plugins/start.py
+++ VideoEncoder/plugins/start.py
@@ -33,7 +33,7 @@ async def start_message(app, message):
     if not c:
         return
     await AddUserToDatabase(app, message)
-    text = f"Hi {message.from_user.mention()}<a href='https://ibb.co/RGx4RmDg'>!</a> I'm VideoEncoder Bot which will do magic with your file."
+    text = f"Hi {message.from_user.mention()}<a href='https://t.me/bosshubots'>!</a> I'm VideoEncoder Bot which will do magic with your file."
     await message.reply(text=text, reply_markup=start_but)
 
 
@@ -70,7 +70,7 @@ For Owner:
 - /addchat and /addsudo
 - /rmsudo and /rmchat
 
-Supports: <a href='https://t.me/cantarellabots'>click here</a>"""
+Supports: <a href='https://t.me/bosshubots'>click here</a>"""
     await message.reply(text=msg, disable_web_page_preview=True, reply_markup=start_but)
 
 
